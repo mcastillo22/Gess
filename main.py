84b0db890c580127e079e220a5fb4c106546384e
Marissa Castillo <mmcastillo22@gmail.com>
2020-06-08 17:26:47 -0700

    improve user functionality for invalid moves
    improve user functionality for invalid moves

--- main.py
+++ main.py
@@ -9,22 +9,30 @@ def main():
     running = True
 
     while running:
-        print("== " + game.get_player().title() + "'s turn ==")
-        print("Piece to move: ", end="")
-        starting_center = input().lower()
-        print("Destination center: ", end="")
-        ending_center = input().lower()
-        
-        quit_options = ['q', 'quit', 0]
 
-        if  starting_center in quit_options or ending_center in quit_options:
-            print("Thanks for playing!")
-            running = False
-        
-        else:
-            game.make_move(starting_center, ending_center)
+        def quit(input):
+            quit_options = ['q', 'quit', 0]
+            if  input in quit_options:
+                print("Thanks for playing!")
+                return False
+            return True
+
+        print("== " + game.get_player().title() + "'s turn ==")
+        print("Center of Piece to Move: ", end="")
+        starting_center = input().lower()
+        if not quit(starting_center):
+            break
+        print("Destination Center: ", end="")
+        ending_center = input().lower()
+        if not quit(ending_center):
+            break
+
+        if game.make_move(starting_center, ending_center):
             game.print_board()
             print()
+        
+        else:
+            print("Invalid move!\n")
 
 
 if __name__ == "__main__":
