4aaabb3f073f41e49c2c51b1c002d543860fad04
Marissa Castillo <mmcastillo22@gmail.com>
2020-06-26 10:36:26 -0700

    clean up constants

--- constants.py
+++ constants.py
@@ -4,21 +4,26 @@ HL          = (255, 255, 200)
 WHITECOLOR  = (255, 255, 255)
 GREY        = (128, 128, 128)
 
-# Coordinates
+# Sizes
 WINDOW_SIZE = (700, 700)
 ARRAY = 20
 SPACE = 30
-
-X1, Y1 = 50, 50
 PIECE = SPACE * 3
+BUTTON_SIZE = 25
+
+# Coordinates
+X1, Y1 = 50, 50
 PIECE_POS = X1 + 3
 
 TURN_SPACE = int(X1 + SPACE*7.5)
 TURN_HEIGHT = int(SPACE*0.45)
 
-BOARD_ARRAY = [x for x in range(X1, (ARRAY+1)*SPACE, SPACE)]
+NEW_GAME_X = int(X1 + SPACE*15.5)
 
 WINNER = X1 + SPACE*5, Y1 + SPACE*7
+
+# Board Array
+BOARD_ARRAY = [x for x in range(X1, (ARRAY+1)*SPACE, SPACE)]
 
 # Strings
 TITLE = 'Gess Game'
@@ -43,4 +48,3 @@ E       = 'E'
 NW      = 'NW'
 N       = 'N'
 NE      = 'NE'
-
